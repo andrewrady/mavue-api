3cdb2a15ff8881f5478224a0897e443624ca5e79
Andrew Rady <andrewrady@Andrews-MacBook-Air.local>
2018-01-20 09:21:33 -0700

    init project with student model

---
+++ db/migrate/20180120161642_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.string :rank
+      t.integer :ata_number
+
+      t.timestamps
+    end
+  end
+end
