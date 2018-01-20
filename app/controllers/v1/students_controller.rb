a827d0432c17e8777080db04125d072f59762438
Andrew Rady <andrewrady@Andrews-MacBook-Air.local>
2018-01-20 10:03:17 -0700

    basic crud for students

--- app/controllers/v1/students_controller.rb
+++ app/controllers/v1/students_controller.rb
@@ -1,7 +1,45 @@
 class V1::StudentsController < ApplicationController
 
   def index
-    @ring = ['test']
-    render json: @ring
+    @student = Student.all
+    render json: @student
   end
+
+  def show
+    @student = Student.where(:id => params[:id])
+
+    render json: @student
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      render json: @student, status: 201
+    else
+      render json: { errors: error.message }, status: 422
+    end
+  end
+
+  def update
+    @student = Student.where(:id => params[:id])
+
+    if @student.update(student_params)
+      render json: @student, status: 201
+    else
+      render json: { errors: errors.message }, status: 422
+    end
+  end
+
+  def destroy
+    @student = Student.where(:id => params[:id]).first
+    @student.destroy
+
+    head 204
+  end
+
+  private
+    def student_params
+      params.permit(:first_name, :last_name, :address, :city, :state, :zip, :rank, :ata_number)
+    end
 end
