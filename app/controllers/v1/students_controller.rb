963331d710a04ee3f831e3e8f71a8b0ab1db84f9
Andrew Rady <andrewrady@Andrews-Air.hitronhub.home>
2018-01-21 21:21:53 -0700

    added devise and knock for auth

--- app/controllers/v1/students_controller.rb
+++ app/controllers/v1/students_controller.rb
@@ -1,4 +1,5 @@
 class V1::StudentsController < ApplicationController
+  before_action :authenticate_user
 
   def index
     @student = Student.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :user_token
 
   namespace :v1 do
     resources :students
